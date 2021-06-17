136591a08a2a7fe9f7c4175f0bc38b7ac1bb7b0c
박성준 <seong_jun@jun.local>
2021-06-17 16:46:02 +0900

    Add: Djangoproject setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wsb',
+        'USER': 'root',
+        'PASSWORD': 'qkzpahshfl1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRECT_KEY = 'asdasdasd'
